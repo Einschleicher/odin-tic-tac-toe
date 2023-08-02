7e9b7bd8e69bf6aed8d70ad7e1d3bbf1a4728869
Einschleicher <116537717+Einschleicher@users.noreply.github.com>
2023-08-02 06:00:49 +0200

    Add render function for display controller

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const gameBoard = {
-    gameBoardArray: ["X", "O", "X", "O", "X", "O", "X", "O", "X"],
+    Array: ["X", "O", "X", "O", "X", "O", "X", "O", "X"],
 };
 
 const displayController = {
-
+    fields: document.querySelectorAll(".board-field"),    
+    renderBoard: () => {
+        gameBoard.Array.forEach((element, index) => {
+            displayController.fields[index].textContent = element;
+        });
+    },
 };
 
 const playerFactory = (name) => {
