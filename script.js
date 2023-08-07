9fecdfe4ad5e74ff63165ef3886d83fc3c04a4c2
Einschleicher <116537717+Einschleicher@users.noreply.github.com>
2023-08-07 14:22:37 +0200

    Final major code optimization and cleanup

--- script.js
+++ script.js
@@ -1,79 +1,110 @@
+const playerFactory = (name, marker) => {
+    return { name, marker };
+};
+
 const gameBoard = (function() {
 
-    const array = ["", "", "", "", "", "", "", "", ""];
+    let array = ["", "", "", "", "", "", "", "", ""];
 
     function addMarker(symbol, position) {
-        if (gameBoard.array[position] === "") {
-            gameBoard.array[position] = symbol;
-            displayController.renderBoard();
-            runCheckWinner();
-            if (endGame === false) displayController.changeCurrentPlayer();
-        }
-        else alert("Error! Field has already been taken");
-
+        array[position] = symbol;
     }
 
-    function runCheckWinner() {
-        displayController.checkWinner(0,1,2);
-        displayController.checkWinner(3,4,5);
-        displayController.checkWinner(6,7,8);
-        displayController.checkWinner(0,3,6);
-        displayController.checkWinner(1,4,7);
-        displayController.checkWinner(2,5,8);
-        displayController.checkWinner(0,4,8);
-        displayController.checkWinner(2,4,6);
+    function clearArray() {
+        array = ["", "", "", "", "", "", "", "", ""]; 
     }
 
-    return { array, addMarker };
+    const getArray = () => array;
+
+    return { addMarker, clearArray, getArray };
 
 })();
 
 const displayController = (function() {
+
+    const playerOne = playerFactory("Player 1", "X");
+    const playerTwo = playerFactory("Player 2", "O");
+
+    let endGame = false;
+    let currentPlayer = playerOne;
+    let board = gameBoard.getArray();
     
     const marker = document.querySelectorAll(".field-marker");
     
     function renderBoard() {
-        gameBoard.array.forEach((element, index) => {
+        board.forEach((element, index) => {
             marker[index].textContent = element;         
         });
     }
 
-    const fields = document.querySelectorAll(".board-field");
-
-    // EventListeners
-    fields.forEach((element, index) => {
-        element.addEventListener("click", () => {
-            if (endGame === false) gameBoard.addMarker(currentPlayer.marker, index);
-        })
-    });
+    const winner = document.querySelector("#winner-display");
 
     function changeCurrentPlayer() {
-        if (currentPlayer === playerOne) currentPlayer = playerTwo;
-        else currentPlayer = playerOne;
+        currentPlayer = (currentPlayer === playerOne) ? playerTwo : playerOne;
         winner.textContent = `${currentPlayer.name}, its your turn!`;
     }
 
-    const winner = document.querySelector("#winner-display");
-
     function checkWinner(indexOne, indexTwo, indexThree) {
-        if (gameBoard.array[indexOne] === gameBoard.array[indexTwo]
-            && gameBoard.array[indexTwo] === gameBoard.array[indexThree]) {
-                if (gameBoard.array[indexOne] !== "") {
+        if (board[indexOne] === board[indexTwo]
+            && board[indexTwo] === board[indexThree]) {
+                if (board[indexOne] !== "") {
                     winner.textContent = `${currentPlayer.name} has won the game! gg wp`;
                     endGame = true;
                 }
         }
     }
 
+    function runCheckWinner() {
+        checkWinner(0,1,2);
+        checkWinner(3,4,5);
+        checkWinner(6,7,8);
+        checkWinner(0,3,6);
+        checkWinner(1,4,7);
+        checkWinner(2,5,8);
+        checkWinner(0,4,8);
+        checkWinner(2,4,6);
+    }
+
+    function checkTie() {
+        if (counter === 9 && endGame === false) {
+            winner.textContent = `It's a tie! Fair game.`;
+            endGame = true;
+        }
+    }
+
+    const fields = document.querySelectorAll(".board-field");
+    let counter = 0;
+
+    // Run turn when a player clicks on a field
+    fields.forEach((element, index) => {
+        element.addEventListener("click", () => {
+            // Cancel turn if field has already been taken
+            if (board[index] !== "" && endGame === false) {
+                alert("Error! Field has already been taken");
+            }
+            // Else execute turn
+            else if (endGame === false) {
+                gameBoard.addMarker(currentPlayer.marker, index);
+                counter++;
+                renderBoard();
+                checkTie();
+                runCheckWinner(); // may turn endGame into true, so double check next line
+                if (endGame === false) changeCurrentPlayer();
+            }
+        })
+    });
+
     const restartButton = document.querySelector("#restart");
 
-    // Restart Game
+    // Restart the game when a player clicks on the restart button
     restartButton.addEventListener("click", () => {
-        gameBoard.array = ["", "", "", "", "", "", "", "", ""];
-        displayController.renderBoard();
+        gameBoard.clearArray();
+        board = gameBoard.getArray();
+        renderBoard();
         currentPlayer = playerOne;
         winner.textContent = `${currentPlayer.name}, its your turn!`;
         endGame = false;
+        counter = 0;
     });
 
     const playerOneButton = document.querySelector("#name-player-one");
@@ -89,16 +120,7 @@ const displayController = (function() {
         winner.textContent = `${currentPlayer.name}, its your turn!`;
     });
 
-    return { renderBoard, changeCurrentPlayer, checkWinner };
+    // No need to return anything in this module, all encapsulated
+    // return { };
 
 })();
-
-const playerFactory = (name, marker) => {
-    return { name, marker };
-};
-
-const playerOne = playerFactory("Player 1", "X");
-const playerTwo = playerFactory("Player 2", "O");
-
-let currentPlayer = playerOne;
-let endGame = false;
